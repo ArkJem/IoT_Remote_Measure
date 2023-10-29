3143bbe2addc9889dd361428b56f7a6f661ca6c7
Arkadiusz Jemioło <asdfghjui1@gmail.com>
2023-10-29 14:58:14 +0100

    Made Login and Register backend service.

---
+++ server/Temperature_Measurement/src/main/java/com/example/Temperature_Measurement/auth/AuthenticationResponse.java
@@ -0,0 +1,14 @@
+package com.example.Temperature_Measurement.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthenticationResponse {
+    private String token;
+}
---
+++ server/Temperature_Measurement/src/main/java/com/example/Temperature_Measurement/users/Role.java
@@ -0,0 +1,6 @@
+package com.example.Temperature_Measurement.users;
+
+public enum Role {
+    USER,
+    ADMINISTRATOR
+}
