8ffd963177fa7da3d09a52afb673892fba3c9c4a
Arkadiusz Jemioło <asdfghjui1@gmail.com>
2023-11-06 09:56:43 +0100

    front Prodct

--- frontend/temp_measurement/src/components/FrontPg/FrontPage.js
+++ frontend/temp_measurement/src/components/FrontPg/FrontPage.js
@@ -1,8 +1,13 @@
 import {useTypingEffect} from "./hooks/typing-effect";
 import "./FrontPage.css"
+import {Button} from "@mui/material";
 export default function FrontPage(){
     const text = useTypingEffect("ZDALNY POMIAR TEMPERATURY",150);
     return(
-        <div className={"frontPg"}>{text}</div>
+            <div className={"frontPg"}>{text}
+            <div>
+                <Button className={"btn"} variant="contained" href={"/login"}>Zaloguj się </Button>
+            </div>
+            </div>
     )
 }
