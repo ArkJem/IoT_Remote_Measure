b9c64d92257b27b7d5d3051cb27a70b1d18eae07
MrFlamePL <asdfghjui1@gmail.com>
2023-11-21 15:23:28 +0100

    edited front page

--- frontend/temp_measurement/src/components/FrontPg/FrontPage.js
+++ frontend/temp_measurement/src/components/FrontPg/FrontPage.js
@@ -4,10 +4,32 @@ import {Button} from "@mui/material";
 export default function FrontPage(){
     const text = useTypingEffect("ZDALNY POMIAR TEMPERATURY",150);
     return(
-            <div className={"frontPg"}>{text}
-            <div>
-                <Button className={"btn"} variant="contained" href={"/login"}>Zaloguj się </Button>
+        <div className={'container'}>
+            <div className='topMenu'>
+                <div className={"table"}>
+                <table>
+                    <tr>
+                        <th><a id={'link'} href="/">Strona Główna</a></th>
+                        <th><a id={'link'} href={"/login"}>Zaloguj się</a></th>
+                        <th><a id={'link'} href={""}>Kontakt</a></th>
+                    </tr>
+                </table>
+                </div>
             </div>
+
+            <div className={'main'}>
+                dupa
             </div>
+
+            <div className={'afterMain'}>
+                dupa
+            </div>
+
+            <div className={'footer'}>
+                dupa
+            </div>
+
+
+        </div>
     )
 }
